66b7e11fe822bde3128c6bbabd1fd51af9493c60
= <tampham.dev@gmail.com>
2019-05-22 15:16:58 +0700

    Test

--- api/server.js
+++ api/server.js
@@ -10,13 +10,36 @@ const app = express();
 const path = require("path");
 const User = require("./model.js");
 const Data = require("./test.js");
-app.options("*", cors());
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-app.use(
-  cors({
-    credentials: true
-  })
-);
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+// app.options("*", cors());
+
+// app.use(
+//   cors({
+//     credentials: true,
+//   })
+// );
 app.use(bodyParser.json()); // for parsing application/json
 app.use(
   bodyParser.urlencoded({
