9a2af7dd623b48e1e950fde867fb583eac8521b4
= <tampham.dev@gmail.com>
2019-05-22 14:06:39 +0700

    save

--- src/service.js
+++ src/service.js
@@ -1,22 +1,26 @@
-import Axios from 'axios';
+import Axios from "axios";
 require("dotenv").config();
-const BASE_URL = process.env.BASE_URL || "http://localhost:5000";
+const BASE_URL = "https://my-react-trello.herokuapp.com";
 
 export function getBoard() {
-  return Axios.get(`${BASE_URL}/api/board`, {withCredentials:true})
-    .then(response => response.data);
+  return Axios.get(`${BASE_URL}/api/board`, { withCredentials: true }).then(
+    response => response.data
+  );
 }
 
 export function login(data) {
-  return Axios.post(`${BASE_URL}/api/login`,   data)
+  return Axios.post(`${BASE_URL}/api/login`, data)
     .then(response => {
-      localStorage.setItem('x-access-token', response.data.token);
-      localStorage.setItem('x-access-token-expiration', Date.now() +2 *60*60 * 1000);
-      localStorage.setItem('user', response.data.username);
+      localStorage.setItem("x-access-token", response.data.token);
+      localStorage.setItem(
+        "x-access-token-expiration",
+        Date.now() + 2 * 60 * 60 * 1000
+      );
+      localStorage.setItem("user", response.data.username);
       console.log(response.data);
       return response.data;
     })
-    .catch(err => Promise.reject('Authentication failed'));
+    .catch(err => Promise.reject("Authentication failed"));
 }
 
 export async function signup(data) {
@@ -24,9 +28,12 @@ export async function signup(data) {
     .then(response => {
       console.log(response.data);
     })
-    .catch(err => Promise.reject('Sign Up failed'));
+    .catch(err => Promise.reject("Sign Up failed"));
 }
 
-export function isAuthenticated(){
-	return localStorage.getItem('x-access-token') && localStorage.getItem('x-access-token-expiration') > Date.now()
+export function isAuthenticated() {
+  return (
+    localStorage.getItem("x-access-token") &&
+    localStorage.getItem("x-access-token-expiration") > Date.now()
+  );
 }
