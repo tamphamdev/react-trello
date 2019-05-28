5e1d9d5966234951f0a841b983e6360075c68ccf
= <tampham.dev@gmail.com>
2019-05-28 17:46:25 +0700

    .

--- api/server.js
+++ api/server.js
@@ -34,9 +34,12 @@ mongoose
 ///^\/(?!api).*/
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "build/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../build/index.html"));
   });
+  // app.get(/^\/(?!api).*/, (req, res) => {
+  //   res.sendFile(path.join(__dirname, "build/index.html"));
+  // });
 }
 app.use("/api", userRoutes);
 app.use("/api", indexRoutes);
