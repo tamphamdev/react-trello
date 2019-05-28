fb1065d78d16437fbae5b9943e7fb251d06c36cb
= <tampham.dev@gmail.com>
2019-05-28 09:24:11 +0700

    Add Reset, Updatepassword function, fix get token from Localstorage

--- src/service.js
+++ src/service.js
@@ -31,8 +31,32 @@ export async function signup(data) {
 }
 
 export function isAuthenticated() {
-  return localStorage.getItem("x-access-token") &&
+ let auth = localStorage.getItem("x-access-token") &&
     localStorage.getItem("x-access-token-expiration") > Date.now()
     ? true
     : false;
+    console.log('auth from serivce :', auth);
+    return auth;
+}
+
+export async function sendEmail(data) {
+  return await Axios.post("/api/reset-password", data)
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => console.log("err :", err));
+}
+
+export async function reset(data) {
+  return await Axios.get("/api/reset", {
+    params: { resetPasswordToken: data }
+  }).then(response => {
+    return response.data;
+  });
+}
+
+export async function updatePassword(data) {
+  return await Axios.put("/api/update-password", data).then(response => {
+    return response.data;
+  });
 }
