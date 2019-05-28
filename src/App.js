8eab19c9179992fdd099c9420af66e752790a791
= <tampham.dev@gmail.com>
2019-05-28 09:24:40 +0700

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
 import React from "react";
 import "antd/dist/antd.css";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Template from "./components/Template";
 import GroupList from "./components/GroupList";
-
+import ModalForgotPass from "./components/ModalForgotPass";
+import ConfirmPassword from "./components/ConfirmPassword";
 function App() {
- 
   return (
     <div className="App">
-      <Template>
-          <GroupList />
-      </Template>
+      <Router>
+        <Template>
+          <Route exact path="/" component={GroupList} />
+          <Route path="/reset-password" component={ModalForgotPass} />
+          <Route path="/confirm-password/:token" component={ConfirmPassword} />
+        </Template>
+      </Router>
     </div>
   );
 }
