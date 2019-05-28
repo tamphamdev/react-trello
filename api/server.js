f6b79fc8dff4edb0cf0469313a68714b7e7aa399
= <tampham.dev@gmail.com>
2019-05-28 09:34:39 +0700

    Fix mini bug

--- api/server.js
+++ api/server.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
-const userRoutes = require('./routes/user.route');
-const indexRoutes = require('./routes/index.route');
+const userRoutes = require("./routes/user.route");
+const indexRoutes = require("./routes/index.route");
 const mongoose = require("mongoose").set("debug", true);
 
 /* Initial CORS before boyParser*/
@@ -16,7 +16,7 @@ app.use(
   })
 );
 // for parsing application/json
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -24,8 +24,8 @@ app.use(
 );
 /* Connect to Mongo DB*/
 mongoose
-.connect(process.env.MONGO_URI || process.env.MONGO_LOCAL, {
-  useNewUrlParser: true
+  .connect(process.env.MONGO_URI || process.env.MONGO_LOCAL, {
+    useNewUrlParser: true
   })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
@@ -38,10 +38,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
-
-app.use('/api', userRoutes);
-app.use('/api', indexRoutes);
+app.use("/api", userRoutes);
+app.use("/api", indexRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("api runnging on port " + PORT + ": "));
