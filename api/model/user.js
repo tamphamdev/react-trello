f45a595b014aa6ed7f829f551122a5de59b6d92e
= <tampham.dev@gmail.com>
2019-05-28 09:25:24 +0700

    Add fields: resetPasswordToken & resetPasswordExpired

--- api/model/user.js
+++ api/model/user.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  resetPasswordToken: String,
+  resetPasswordExpired: String
 });
 
 module.exports = mongoose.model("user", userSchema);
