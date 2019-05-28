0218c0f058e880e99a841a70977a9717dd03803f
= <tampham.dev@gmail.com>
2019-05-28 13:45:04 +0700

    .

--- api/server.js
+++ api/server.js
@@ -34,7 +34,7 @@ mongoose
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/build", "/index.html"));
+    res.sendFile(path.join(__dirname, "build/index.html"));
   });
 }
 // if (process.env.NODE_ENV === "production") {
