3735bfd3b2b8097e892906507953ce67f0b14b19
= <tampham.dev@gmail.com>
2019-05-28 09:16:56 +0700

    Removed footer Modal Login

--- src/components/ModalLogin.js
+++ src/components/ModalLogin.js
@@ -1,71 +1,67 @@
-import React, { Component } from 'react';
-import {Modal, Button, Typography} from 'antd';
-import FormLogin from './FormLogin';
-
-const {Text} = Typography;
-
+import React, { Component } from "react";
+import { Modal, Typography } from "antd";
+import FormLogin from "./FormLogin";
+const { Text } = Typography;
 
 export default class ModalLogin extends Component {
-
   state = {
-    ModalText: 'Content of the modal',
+    ModalText: "Content of the modal",
     visible: false,
     confirmLoading: false
-  }
+  };
   showModal = () => {
     this.setState({
-      visible: true,
+      visible: true
     });
   };
 
   // submit form
-  handleSubmit = (e) => {
-
-      this.setState({
-        ModalText: 'Logging....Please wait a second ',
-        confirmLoading: true,
-      });
-      setTimeout(() => {
-        this.props.action();
-        this.setState({
-          visible: false,
-          confirmLoading: false,
-        });
-      }, 500);
-  }
-  
-  handleCancel = () => {
-    console.log('Clicked cancel button');
+  handleSubmit = e => {
     this.setState({
-      visible: false,
+      ModalText: "Logging....Please wait a second ",
+      confirmLoading: true
+    });
+    setTimeout(() => {
+      this.props.action();
+      this.setState({
+        visible: false,
+        confirmLoading: false
+      });
+    }, 500);
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false
     });
   };
 
   render() {
-     const { visible, confirmLoading  } = this.state;
+    const { visible, confirmLoading } = this.state;
     return (
       <div>
-        <div type="primary" onClick={this.showModal} >
-        <Text strong style={{ color: "#fff" }}>Login</Text>
+        <div type="primary" onClick={this.showModal}>
+          <Text strong style={{ color: "#fff" }}>
+            Login
+          </Text>
         </div>
         <Modal
-        title="Login Form"
-        visible={visible}
-        onOk={this.handleSubmit}
-        confirmLoading={confirmLoading}
-        onCancel={this.handleCancel}
-        footer={[
-          <Button form="myform" key="submit" htmlType="submit" onClick={this.handleSubmit}
-          onKeyUp={this.handleSubmit}>
-              Submit
-          </Button>
-          ]}
+          footer={null}
+          title="Login Form"
+          visible={visible}
+          onOk={this.handleSubmit}
+          confirmLoading={confirmLoading}
+          onCancel={this.handleCancel}
         >
-          <FormLogin {...this.state}  {...this.props}/>
+          <FormLogin
+            {...this.state}
+            {...this.props}
+            closeModal={this.handleSubmit}
+            handleCancel={this.handleCancel}
+          />
         </Modal>
       </div>
-    )
+    );
   }
-};
+}
 
-// }
