5e61e26f6d88e4ee9ef247b64baa780fc4ed5f22
= <tampham.dev@gmail.com>
2019-05-28 14:30:10 +0700

    .

--- api/server.js
+++ api/server.js
@@ -34,8 +34,8 @@ mongoose
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname + "../build/index.html"));
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build/index.html"));
   });
 }
 
