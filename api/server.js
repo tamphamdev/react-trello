1e62218619b706716b4e5ecdee1638bfc0acb5f0
= <tampham.dev@gmail.com>
2019-05-28 21:05:30 +0700

    .

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ mongoose
 /* Serve static file if in production*/
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  app.get("/", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "build/index.html"));
   });
 }
