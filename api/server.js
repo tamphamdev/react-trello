c57e30ab0162a9572e30ab5455f17cb20a0cf9a9
= <tampham.dev@gmail.com>
2019-05-28 12:25:29 +0700

    .

--- api/server.js
+++ api/server.js
@@ -4,19 +4,19 @@ const cors = require("cors");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
-const userRoutes = require('./routes/user.route');
-const indexRoutes = require('./routes/index.route');
+const userRoutes = require("./routes/user.route");
+const indexRoutes = require("./routes/index.route");
 const mongoose = require("mongoose").set("debug", true);
 
 /* Initial CORS before boyParser*/
 app.options("*", cors());
 app.use(
   cors({
-    credentials: true,
+    credentials: true
   })
 );
 // for parsing application/json
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -24,12 +24,19 @@ app.use(
 );
 /* Connect to Mongo DB*/
 mongoose
-.connect(process.env.MONGO_URI || process.env.MONGO_LOCAL, {
-  useNewUrlParser: true
+  .connect(process.env.MONGO_URI || process.env.MONGO_LOCAL, {
+    useNewUrlParser: true
   })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
+app.get("/*", (req, res) => {
+  let url = path.join(__dirname, "build/index.html");
+  if (!url.startsWith("/app/"))
+    // we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
 /* Serve static file if in production*/
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
@@ -38,10 +45,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
-
-app.use('/api', userRoutes);
-app.use('/api', indexRoutes);
+app.use("/api", userRoutes);
+app.use("/api", indexRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("api runnging on port " + PORT + ": "));
